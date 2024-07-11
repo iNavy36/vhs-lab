f17cc3c2a1c8c827da8dc17547449700e75a4006
iNavy36 <ivan_22_mihovilovic@hotmail.com>
2024-07-12 00:16:54 +0200

    made user as JPA entity, inserted default values into created table

---
+++ spring-boot/src/main/java/com/example/lab/model/UserEntity.java
@@ -0,0 +1,53 @@
+package com.example.lab.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "user_email")
+    private String email;
+
+    public UserEntity() {
+
+    }
+
+    public UserEntity(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ spring-boot/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (user_name, user_email) VALUES
+    ('Admin Admin','admin@mail.com'),
+    ('First User', 'user1@mail.com');
